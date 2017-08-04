db04c97a1e629520340a80155331f701112da204
Jan Parthemore <janparthemore@gmail.com>
2017-08-03 21:01:07 -0400

    added home page component header. Styled nav bar to match header image on main component - no longer using bootstrap.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@
 const angular = require('angular');
 require('bootstrap/dist/css/bootstrap.css');
 require('font-awesome/css/font-awesome.css');
+require('./normalize.css');
 
 const NavbarComponent = require('./components/nav-bar');
+const HomePageComponent = require('./components/home-page');
 
 //create our applicaton
 angular.module('creative-corner',[])
+       .component('homePage',HomePageComponent)
        .component('navBar',NavbarComponent);
---
+++ src/components/home-page/home-page.controller.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6*/
+
+function HomePageController(){
+  this.message = "this is home view";
+}
+
+HomePageController.$inject = [];
+
+module.exports = HomePageController;
