7e48b2431eec8821c7867ded3e649c94ce5aeb4a
Jan Parthemore <janparthemore@gmail.com>
2017-08-15 18:55:47 -0400

    added description for category to page. Misc styling changes.

--- src/components/paper/paper.controller.js
+++ src/components/paper/paper.controller.js
@@ -4,6 +4,12 @@ function PaperController(ProductService, $location){
   this.paperinputtypes =['Printed Paper','Cardstock Chalky Stripes'];
   this.paperinput = chosenpapertype||this.paperinputtypes[0];
   this.addToCart = addToCart;
+  // this.total = 0;
+  // this.updateTotal = updateTotal;
+
+  // function updateTotal(){
+  //    console.log('updating totals');
+  // }
 
   this.onChange = function onChange(){
     chosenpapertype = this.paperinput;
@@ -12,8 +18,6 @@ function PaperController(ProductService, $location){
                   .then(() => $location.path(`/paper/${category}`))
                   .catch(() => console.error('path not found'));
   };
-
-
 
   function addToCart(){
     console.log('adding to cart');
